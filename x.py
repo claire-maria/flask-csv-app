0140e17c3d064430fcf2e5e69bb30adb7a6db60c
claire <cgribbin@tcd.ie>
2021-03-23 10:10:13 +0000

    remove files

--- x.py
+++
@@ -1,9 +0,0 @@
-import datetime
-import pandas as pd
-
-col_names = ['guid','name','first', 'last', 'email' , 'value', 'date', 'phone', 'age', 'state', 'street']
-csvData = pd.read_csv('/home/claire/calypso/venv/example.csv' ,names=col_names, header=None)
-x = 0
-for i,row in csvData.iterrows():
-    x = x + 1
-print(x)
